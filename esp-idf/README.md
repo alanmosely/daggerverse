8e076257b8dbe2298ae46b2c6cf5845c75715356
alanmosely <alan.mosely@gmail.com>
2024-09-14 23:17:12 +0100

    First cut of an esp32 idf.py module

---
+++ esp-idf/README.md
@@ -0,0 +1,26 @@
+# Daggerverse ESP-IDF Module
+
+[Dagger](https://dagger.io/) module for [daggerverse](https://daggerverse.dev/), providing [Espressif ESP-IDF](https://github.com/espressif/esp-idf) idf.py functionality.
+
+## Usage
+
+Check the official Dagger Module documentation: <https://docs.dagger.io/>
+
+The [Dagger CLI](https://docs.dagger.io/cli) is a pre-requisite.
+
+### Functions
+
+List all functions of the module. This command is provided by the [Dagger CLI](https://docs.dagger.io/reference/cli/).
+
+```bash
+dagger functions
+```
+
+## Development
+
+### Set up Dagger module
+
+```bash
+# initialize the module
+dagger develop --sdk python
+```
--- esp-idf/src/main/esp_idf.py
+++ esp-idf/src/main/esp_idf.py
@@ -1,5 +1,12 @@
+"""The ESP-IDF (Espressif IoT Development Framework) is a software development framework for the ESP32 chip series by Espressif.
+
+The idf.py command-line tool provides a front-end to easily manage your project builds, deployment, debugging and more. It manages CMake, Ninja and the esptool.py.
+"""
+
+from typing import Annotated
+
 import dagger
-from dagger import dag, function, object_type
+from dagger import dag, Doc, function, object_type
 
 
 @object_type
@@ -7,12 +14,20 @@ class EspIdf:
     @function
     async def build(
         self,
-        project_dir: dagger.Directory,
-        image_version: str = "v5.1",
-        idf_args: list = ["build"],
+        project_dir: Annotated[
+            dagger.Directory, Doc("The directory containing the ESP-IDF project")
+        ],
+        image_version: Annotated[
+            str, Doc("The version of the Espressif IDF Docker image to use")
+        ] = "v5.1",
+        idf_args: Annotated[list[str], Doc("The arguments to pass to idf.py")] = [
+            "build"
+        ],
     ) -> str:
-        """Executes idf.py from the ESP-IDF Docker image"""
+        """Execute idf.py from the official Espressif IDF Docker image
 
+        Example usage: dagger call build --project_dir . --image_version "v5.2"
+        """
         build_output = await (
             dag.container()
             .from_(f"espressif/idf:{image_version}")
